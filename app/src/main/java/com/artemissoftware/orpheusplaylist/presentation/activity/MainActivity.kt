34b96dab9346d519cbf0515db9320d98b18e078d
gustavo <gustavo.isaac@gmail.com>
2023-02-21 17:29:59 +0000

    AudioPlayerState

--- app/src/main/java/com/artemissoftware/orpheusplaylist/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/presentation/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.artemissoftware.orpheusplaylist.presentation.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,9 +16,18 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mainViewModel.loadMedias()
+
         setContent {
+
+
+
             OrpheusPlaylistTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
--- app/src/main/java/com/artemissoftware/orpheusplaylist/presentation/activity/MainViewModel.kt
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/presentation/activity/MainViewModel.kt
@@ -3,22 +3,55 @@ package com.artemissoftware.orpheusplaylist.presentation.activity
 import android.content.Context
 import android.media.MediaPlayer
 import android.net.Uri
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.artemissoftware.orpheusplaylist.domain.model.AudioMetadata
+import com.artemissoftware.orpheusplaylist.domain.repository.AudioPlayerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    //--private val repository: HeadphonePlayerRepository
+    private val repository: AudioPlayerRepository
 ) : ViewModel() {
 
 
     private var _player: MediaPlayer? = null
 
+    init {
+        loadMedias()
+    }
+
     private fun initPlayer(context: Context, uri: Uri) {
         _player = MediaPlayer().apply {
             setDataSource(context, uri)
             prepare()
+        }
+    }
+
+     fun loadMedias() {
+        viewModelScope.launch {
+//            _state = _state.copy(isLoading = true)
+            val audios = mutableStateListOf<AudioMetadata>()
+            val lolo = prepareAudios()
+            audios.addAll(lolo)
+//            _state = _state.copy(audios = audios)
+//            repository.getLikedSongs().collect { likedSongs ->
+//                _state = _state.copy(
+//                    likedSongs = likedSongs,
+//                    isLoading = false,
+//                )
+//            }
+        }
+    }
+
+    private suspend fun prepareAudios(): List<AudioMetadata> {
+        return repository.getAudios().map {
+            val artist = if (it.artist.contains("<unknown>"))
+                "Unknown artist" else it.artist
+            it.copy(artist = artist)
         }
     }
 
