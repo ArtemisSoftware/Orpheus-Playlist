c328c6729843d45a7a81e59f9243b33fd58a0499
gustavo <gustavo.isaac@gmail.com>
2023-02-21 12:13:08 +0000

    OrpheusPlaylistApplication

---
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/OrpheusPlaylistApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.orpheusplaylist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OrpheusPlaylistApplication : Application()
