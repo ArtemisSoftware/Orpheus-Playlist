1b28f715be50081cce5231ce3f99b4ebd01e9216
gustavo <gustavo.isaac@gmail.com>
2023-02-24 10:31:01 +0000

    WarningMessage

---
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/presentation/playlist/composables/WarningMessage.kt
@@ -0,0 +1,62 @@
+package com.artemissoftware.orpheusplaylist.presentation.playlist.composables
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.orpheusplaylist.R
+
+@Composable
+fun WarningMessage(
+    text: String,
+    @DrawableRes iconResId: Int,
+    modifier: Modifier = Modifier,
+    trailingContent: (@Composable () -> Unit)? = null
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth(fraction = 0.90f),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Icon(
+            painter = painterResource(id = iconResId),
+            tint = MaterialTheme.colors.onSurface,
+            contentDescription = ""
+        )
+
+        Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = text,
+                color = MaterialTheme.colors.onSurface,
+                style = MaterialTheme.typography.subtitle2
+            )
+            trailingContent?.invoke()
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun WarningMessagePreview() {
+    WarningMessage(
+        text = "txt_no_media",
+        iconResId = R.drawable.circle_info_solid,
+        modifier = Modifier.padding(vertical = 16.dp)
+    )
+}
