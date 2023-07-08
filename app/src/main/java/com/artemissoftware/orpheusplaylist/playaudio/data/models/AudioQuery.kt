22fb5ca023bb2474792ccd3b234b2717a3cd9713
gustavo <gustavo.isaac@gmail.com>
2023-07-08 09:57:48 +0100

    AudioQuery

---
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/playaudio/data/models/AudioQuery.kt
@@ -0,0 +1,30 @@
+package com.artemissoftware.orpheusplaylist.playaudio.data.models
+
+import android.net.Uri
+import android.provider.MediaStore
+
+data class AudioQuery(
+    val uri: Uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+    val projection: Array<String>,
+    val selectionClause: String?,
+    val selectionArg: Array<String>,
+    val sortOrder: String,
+) {
+
+    companion object {
+
+        val DEFAULT = AudioQuery(
+            projection = arrayOf(
+                MediaStore.Audio.AudioColumns.DISPLAY_NAME,
+                MediaStore.Audio.AudioColumns._ID,
+                MediaStore.Audio.AudioColumns.ARTIST,
+                MediaStore.Audio.AudioColumns.DATA,
+                MediaStore.Audio.AudioColumns.DURATION,
+                MediaStore.Audio.AudioColumns.TITLE,
+            ),
+            selectionClause = "${MediaStore.Audio.AudioColumns.IS_MUSIC} = ?",
+            selectionArg = arrayOf("1"),
+            sortOrder = "${MediaStore.Audio.AudioColumns.DISPLAY_NAME} ASC",
+        )
+    }
+}
