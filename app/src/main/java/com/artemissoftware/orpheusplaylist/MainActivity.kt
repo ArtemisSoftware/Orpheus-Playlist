73ca84aace1102c2199ec745987ee22f39e66e3e
gustavo <gustavo.isaac@gmail.com>
2023-07-10 06:41:02 +0100

    Gradle changes

--- app/src/main/java/com/artemissoftware/orpheusplaylist/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/MainActivity.kt
@@ -10,21 +10,47 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.artemissoftware.orpheusplaylist.playaudio.presentation.AudioPlaylistScreen
+import com.artemissoftware.orpheusplaylist.player.PlayerScreen
 import com.artemissoftware.orpheusplaylist.ui.theme.OrpheusPlaylistTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OrpheusPlaylistTheme {
+                // --RequestPermission(permission = Manifest.permission.READ_EXTERNAL_STORAGE)
+//                val permissionState = rememberPermissionState(
+//                    permission = Manifest.permission.READ_EXTERNAL_STORAGE,
+//                )
+//
+//                val lifecycleOwner = LocalLifecycleOwner.current
+//
+//                DisposableEffect(key1 = lifecycleOwner) {
+//                    val observer = LifecycleEventObserver { _, event ->
+//                        if (event == Lifecycle.Event.ON_RESUME) {
+//                            permissionState.launchPermissionRequest()
+//                        }
+//                    }
+//                    lifecycleOwner.lifecycle.addObserver(observer)
+//
+//                    onDispose {
+//                        lifecycleOwner.lifecycle.removeObserver(observer)
+//                    }
+//                }
+//
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    Greeting("Android")
+                     AudioPlaylistScreen()
+                    //PlayerScreen()
+                    //Greeting("Android")
                 }
             }
         }
