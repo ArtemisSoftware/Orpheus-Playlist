b9619bee2f2aca89deb6c89c807d485f7a1a76fc
gustavo <gustavo.isaac@gmail.com>
2023-07-18 11:38:03 +0100

    Screens

---
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/navigation/Screens.kt
@@ -0,0 +1,34 @@
+package com.artemissoftware.orpheusplaylist.navigation
+
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+sealed class Screens(
+    private val route: String,
+    val arguments: List<NamedNavArgument> = emptyList(),
+) {
+
+    object Albums : Screens(route = "Albums")
+    object Playlist : Screens("Playlist", arguments = listOf(navArgument("albumId") { type = NavType.LongType })) {
+        const val albumId = "albumId"
+    }
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+
+    fun fullRoute(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/{$arg}")
+            }
+        }
+    }
+}
