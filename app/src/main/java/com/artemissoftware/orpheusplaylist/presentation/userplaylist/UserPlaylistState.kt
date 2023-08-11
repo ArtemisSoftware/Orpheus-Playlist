f900d7704269a8d4a32302e1a1603719c4d14d6d
gustavo <gustavo.isaac@gmail.com>
2023-08-11 05:49:16 +0100

    UserPlaylistState

---
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/presentation/userplaylist/UserPlaylistState.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.orpheusplaylist.presentation.userplaylist
+
+import com.artemissoftware.orpheusplaylist.data.models.Album
+
+data class UserPlaylistState(
+    val album: Album? = null,
+)
