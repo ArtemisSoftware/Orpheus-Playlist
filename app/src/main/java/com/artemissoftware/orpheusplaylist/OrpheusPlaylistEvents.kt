41c10f247a309dcfe323af435979ee93163d4478
gustavo <gustavo.isaac@gmail.com>
2023-08-23 04:45:25 +0100

    OrpheusPlaylistEvents

---
+++ app/src/main/java/com/artemissoftware/orpheusplaylist/OrpheusPlaylistEvents.kt
@@ -0,0 +1,20 @@
+package com.artemissoftware.orpheusplaylist
+
+import com.artemissoftware.orpheusplaylist.domain.models.Album
+import com.artemissoftware.orpheusplaylist.domain.models.Audio
+
+sealed class OrpheusPlaylistEvents {
+    data class PlayAudio(val track: Audio) : OrpheusPlaylistEvents()
+    object PlayTrack : OrpheusPlaylistEvents()
+    data class SwipePlayTrack(val track: Audio) : OrpheusPlaylistEvents()
+
+    object SkipToNext : OrpheusPlaylistEvents()
+    object SkipToPrevious : OrpheusPlaylistEvents()
+    data class SeekTo(val value: Float) : OrpheusPlaylistEvents()
+
+    data class PreLoadAlbum(val album: Album) : OrpheusPlaylistEvents()
+
+    data class TogglePlayerDisplay(val isFullDisplay: Boolean) : OrpheusPlaylistEvents()
+
+    data class UpdateUserPlaylist(val audioId: Long) : OrpheusPlaylistEvents()
+}
